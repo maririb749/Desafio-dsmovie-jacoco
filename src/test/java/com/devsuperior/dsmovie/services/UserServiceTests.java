8a8168aea969d48543b3572900c7898ffddfcb46
Mariana Santos <maririb51@gmail.com>
2023-10-30 15:46:22 +0000

    Teste garante que o serviço autentique e retorne corretamente um usuário existente quando necessário.

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,10 +1,21 @@
 package com.devsuperior.dsmovie.services;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -12,9 +23,39 @@ public class UserServiceTests {
 
 	@InjectMocks
 	private UserService service;
+	
+	@Mock
+	private UserRepository repository;
+	
+	@Mock
+	private CustomUserUtil userUtil;
+	
+	private String existingUsername;
+	private String nonExistingUsername;
+	private UserEntity user;
+	
+	@BeforeEach
+	public void setUp()throws Exception  {
+		existingUsername = "maria@gmail.com";
+		nonExistingUsername = "fulano@gmail.com";
+		
+		user = UserFactory.createUserEntity();
+		
+		Mockito.when(repository.findByUsername(existingUsername)).thenReturn(Optional.of(user));
+		Mockito.when(repository.findByUsername(nonExistingUsername)).thenReturn(Optional.empty());
+		
+	}
 
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		
+		Mockito.when(userUtil.getLoggedUsername()).thenReturn(existingUsername);
+		
+
+		UserEntity user = service.authenticated();
+		
+		Assertions.assertNotNull(user);
+		Assertions.assertEquals(user.getUsername(), existingUsername);
 	}
 
 	@Test
